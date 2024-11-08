1725dd1980220086b9c92b17fb4dc3435558a4d6
Eliam LOTONGA <e.m.lotonga@gmail.com>
2024-11-08 15:11:26 +0100

    update balance state

--- src/App.js
+++ src/App.js
@@ -14,9 +14,72 @@ import { useBalance, useIncomeExpense } from "./utils/hooks";
  * @type {Array<{id: Number, name: String, amount: Number, type: 'income' | 'expense'}>}
  */
 const historyData = [
-	{ id: 1, name: "Cash", amount: 1000, type: "income" },
-	{ id: 2, name: "Book", amount: 500, type: "expense" },
-	{ id: 3, name: "Camera", amount: 500, type: "expense" },
+	{
+		id: 1,
+		name: "Cash",
+		amount: 1000,
+		type: "income",
+	},
+	{
+		id: 2,
+		name: "Book",
+		amount: 500,
+		type: "expense",
+	},
+	{
+		id: 3,
+		name: "Camera",
+		amount: 500,
+		type: "expense",
+	},
+	{
+		name: "Printer",
+		amount: 566,
+		type: "expense",
+		id: 4,
+	},
+	{
+		name: "Car",
+		amount: 15000,
+		type: "expense",
+		id: 5,
+	},
+	{
+		name: "TV",
+		amount: 1255,
+		type: "expense",
+		id: 6,
+	},
+	{
+		name: "Computer",
+		amount: 1200,
+		type: "expense",
+		id: 7,
+	},
+	{
+		name: "Portable computer",
+		amount: 899,
+		type: "expense",
+		id: 8,
+	},
+	{
+		name: "Playstation 5",
+		amount: 550,
+		type: "expense",
+		id: 9,
+	},
+	{
+		name: "XBox Serie X",
+		amount: 499,
+		type: "expense",
+		id: 10,
+	},
+	{
+		name: "ReactJS book",
+		amount: 49,
+		type: "expense",
+		id: 11,
+	},
 ];
 
 function App() {
@@ -25,7 +88,7 @@ function App() {
 		transactionReducer,
 		historyData
 	);
-	const [balance, updateBalance] = useBalance(41524);
+	const [balance, updateBalance] = useBalance(20506);
 
 	const [incomeValue, expenseValue] = useIncomeExpense(historyState);
 	/**
